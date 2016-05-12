5de88f391976744553814e3291902868d54d6aa8
轻微 <wyh405003836@126.com>
2016-05-12 19:49:35 +0800

    publish 0.2

--- src/com/dim/utils/Logger.java
+++ src/com/dim/utils/Logger.java
@@ -1,14 +1,12 @@
 package com.dim.utils;
 
 
-import com.dim.ui.NotificationHelper;
-
 /**
  * Created by dim on 16/4/1.
  */
 public class Logger {
 
-    private static boolean debug = true;
+    private static boolean debug = false;
 
     public static void println(String message) {
         if (debug) {
